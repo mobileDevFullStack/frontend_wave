42cd6bebeee50d78beda57a80a5173ab9f12732d
Elzocodeur <gningeli03@gmail.com>
2024-11-18 11:00:48 +0000

    release architecture provider

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 
 const String API_TYPE = 'http'; // Changez en 'dio' pour utiliser Dio au lieu de http
 
-const String BASE_API_URL = 'http://192.168.6.41:4321/api/v1';
+const String BASE_API_URL = 'http://192.168.178.73:4321/api/v1';
 
 const bool useSharedPreferences = true; // Changez en `false` pour utiliser Hive.
